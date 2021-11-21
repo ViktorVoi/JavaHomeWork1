121380e05a43fb3903efd5f192e8bb30e8b4e8a2
Victor Voitovych <victor.voitovych@privatbank.ua>
2021-11-21 10:25:02 +0200

    my HomeTask #8

--- src/com/pb/voitovych/hw8/OnlineShop.java
+++ src/com/pb/voitovych/hw8/OnlineShop.java
@@ -1,8 +1,12 @@
 package com.pb.voitovych.hw8;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class OnlineShop {
     public static void main(String[] args) {
         Auth user = new Auth() ;
+        System.out.println("Start checking...");
         // реєструємся неправильно1
         try {
             user.singUp("НЕПРАВИ;%:?", "123456", "123456");
@@ -34,6 +38,44 @@ public class OnlineShop {
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
-        System.out.println("---- the end");
+        System.out.println("---- the end of checking \n");
+
+        Scanner scan = new Scanner(System.in) ;
+        String login, password, confirmPassword ;
+        boolean success ;
+        do {
+            success = true ;
+            System.out.println("-= Реєстрація користувача =-");
+            System.out.print("Логін (>=5 симв., A-z,0-9): ");
+            login = scan.next();
+            System.out.print("Пароль (> 5 симв., A-z,0-9,_): ");
+            password = scan.next();
+            System.out.print("Підвердіть пароль: ");
+            confirmPassword = scan.next();
+            try {
+                user.singUp(login, password, confirmPassword);
+            } catch (Exception ex) {
+                success = false ;
+                System.out.println(ex.getMessage());
+            }
+        } while (!success) ;
+
+        int attempt = 0 ;
+        do {
+            success = true ;
+            attempt++ ;
+            System.out.println("-= Авторизація користувача =- "+attempt);
+            System.out.print("Логін: ");
+            login = scan.next();
+            System.out.print("Пароль: ");
+            password = scan.next();
+            try {
+                user.singIn(login, password);
+            } catch (Exception ex) {
+                success = false ;
+                System.out.println(ex.getMessage());
+            }
+        } while (!success && attempt <= 3) ;
+
     }
 }
